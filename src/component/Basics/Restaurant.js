3a89698c9d0eef23e6291d6e990b98b79108ff5c
Akshada <Akshada Kadam>
2023-09-01 22:57:03 +0100

    Initial commit

---
+++ src/component/Basics/Restaurant.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import './style.css'
+import Menu from './menuApi'
+import MenuCard from './MenuCard'
+
+
+const uniqueList = [
+  ...new Set(
+    Menu.map((curElem) => {
+      return curElem.category;
+    })
+  ),
+];
+
+console.log(uniqueList);
+
+
+const Restaurant = () => {
+  const [menuData, setMenuData] = useState(Menu);
+
+  const filterItem = (category) => {
+    const updatedList = Menu.filter((curElem) => {
+      return curElem.category  === category;
+  })
+  setMenuData(updatedList)
+  }
+  
+  return (
+    <>
+    <div className='cafe'>
+    <h1>BREEZY CHEEZY</h1>
+    </div>
+    <nav className='navbar'>
+      <div className='btn-group'>
+        <button className='btn-group__item' onClick={() => filterItem("breakfast")}>Breakfast</button>
+        <button className='btn-group__item' onClick={() => filterItem("Afternoon")}>Afternoon</button>
+        <button className='btn-group__item' onClick={() => filterItem("Mocktails")}>Mocktails</button>
+        <button className='btn-group__item' onClick={() => filterItem("Dessert")}>Dessert</button>
+        <button className='btn-group__item' onClick={() => setMenuData(Menu)}>All</button>
+
+
+      </div>
+    </nav>
+
+      <MenuCard menuData={menuData} />
+    </>      
+  )
+}
+
+export default Restaurant
---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,131 @@
+const Menu = [
+    {
+      id: 1,
+      image: "Images/grilledsandwich.jpg",
+      name: "Sandwich",
+      category: "breakfast",
+      price: "50₹",
+      description:
+        "I love Sandwich really oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "Images/omelette.jpg",
+      name: " Omelette",
+      category: "breakfast",
+      price: "25₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "Images/pancake.jpg",
+      name: "Pancake",
+      category: "breakfast",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "Images/smoothie.png",
+      name: "Smoothie",
+      category: "Mocktails",
+      price: "30₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "Images/coldcoffee.jpg",
+      name: "Cold Coffee",
+      category: "Mocktails",
+      price: "100₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 10,
+      image: "Images/mintmojito.jpg",
+      name: "Mint Mojito",
+      category: "Mocktails",
+      price: "150₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "Images/pepperonipizza.jpg",
+      name: "Pepperoni Pizza",
+      category: "Afternoon",
+      price: "70₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "Images/hamburger.jpg",
+      name: "Hamburger",
+      category: "Afternoon",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "Images/vegmomo.png",
+      name: "Veg Momo",
+      category: "Afternoon",
+      price: "40₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "Images/fries.jpg",
+      name: "French Fries",
+      category: "Afternoon",
+      price: "40₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 11,
+      image: "Images/cakes.jpg",
+      name: "Cup Cake",
+      category: "Dessert",
+      price: "40₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 12,
+      image: "Images/caramelcustard.jpg",
+      name: "Caramel Custard",
+      category: "Dessert",
+      price: "40₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 13,
+      image: "Images/chocopastry.jpg",
+      name: "Chocolate Pastry",
+      category: "Dessert",
+      price: "40₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 14,
+      image: "Images/icecream.jpg",
+      name: "Ice Cream",
+      category: "Dessert",
+      price: "40₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  ];
+  
+  export default Menu;
